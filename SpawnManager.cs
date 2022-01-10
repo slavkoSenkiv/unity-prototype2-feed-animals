76ec2d3b5ddc60735acd697d101275552bf74642
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2022-01-10 10:30:42 +0200

    upd

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalsPrefabs;
-    public int animalIndex = 0;
+    private float spawnRange = 20;
+    private float spawnPositionZ = 20;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)){
-            Instantiate(animalsPrefabs[animalIndex], new Vector3(0, 0, 20), animalsPrefabs[animalIndex].transform.rotation);
-        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalsPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPositionZ);
+        Instantiate(animalsPrefabs[animalIndex], spawnPosition, animalsPrefabs[animalIndex].transform.rotation);
+        
     }
 }
